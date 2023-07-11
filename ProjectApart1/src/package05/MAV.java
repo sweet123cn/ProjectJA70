3b30eb20134fcef7f0105d7a7eff97f6bc47aeeb
DavidTang <sweet123cn@outlook.com>
2023-07-11 19:20:32 -0400

    Update MAV.java
    create MAV class

--- ProjectApart1/src/package05/MAV.java
+++ ProjectApart1/src/package05/MAV.java
@@ -1,5 +1,32 @@
 package package05;
 
-public class MAV {
+import package04.UAV;
+
+public class  MAV extends UAV{
+
+    private String brand;
+    private int carryCapacity;
+
+    // default constructor
+    public AgriculturalDrone() {
+        super();
+		this.brand = "AgriFlyer";
+        this.carryCapacity = 50;
+	}
+
+    // parameter constructor
+	public AgriculturalDrone(String brand, double price, int horsepower, double weight,int carryCapacity) {
+		
+        super(brand, price, horsepower,weight);
+		this.brand = brand;
+        this.carryCapacity = carryCapacity;
+	}
+	
+	//copy constructor
+    public AgriculturalDrone(AgriculturalDrone agriculturalDrone) {
+		super(agriculturalDrone);
+		this.brand = agriculturalDrone.brand;
+		this.carryCapacity = agriculturalDrone.carryCapacity;
+	}
 
 }
