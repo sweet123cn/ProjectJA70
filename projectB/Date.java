889392a8615dd8dc1c30dfa203e7e82dbc9514d2
xingr121 <137085577+xingr121@users.noreply.github.com>
2023-07-11 19:09:45 -0400

    commit projectB first version

---
+++ projectB/Date.java
@@ -0,0 +1,67 @@
+package projectB;
+
+public class Date {
+	private int month;
+	private int day;
+	private int year;
+	
+	public Date(int mm, int dd, int yy) {
+		month = mm;
+		day = dd;
+		year = yy;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int mm) {
+		month = mm;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int dd) {
+		day = dd;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int yy) {
+		year = yy;
+	}
+	public  void dateOK() {
+		if(this.getYear() < 1000 || this.getYear() > 9999) {
+			setYear(1900);
+		}
+		if(this.getDay() < 1 || this.getDay() > 31) {
+			setDay(1);
+		}
+		if(this.getMonth() < 1 || this.getMonth() > 12) {
+			setMonth(1);
+		}
+	}
+	
+
+	public  boolean comparedateAfterDate(Date d) {
+		if(this.year > d.year) {
+			return true;
+		}else if (this.year == d.year && this.month > d.month) {
+			return true;
+		}else if (this.year == d.year && this.month == d.month && this.day > d.day) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return month + "-" + day + "-" + year ;
+	}
+	
+}
---
+++ projectB/Employee.java
@@ -0,0 +1,56 @@
+package projectB;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+	private String name;
+	private int id;
+	private Date hireDate;
+	private String position;
+	
+	public Employee(String n, int idNum, Date hDate, String p) {
+		name = n;
+		id = idNum;
+		hireDate = hDate;
+		position = p;	
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int idNum) {
+		id = idNum;
+	}
+
+	public Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(Date hDate) {
+		hireDate = hDate;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String p) {
+		position = p;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee name: " + name + ", id: " + id + ", hireDate: " + hireDate + ", position: " + position;
+	}
+	
+}
