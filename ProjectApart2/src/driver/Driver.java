074826ea3fd7989d598e6c29b1062df08cf43def
lisiCAO <caolisi@hotmail.com>
2023-07-12 15:18:18 -0400

    fix driver program

--- ProjectApart2/src/driver/Driver.java
+++ ProjectApart2/src/driver/Driver.java
@@ -7,6 +7,7 @@ import package03.Multirotor;
 import package04.UAV;
 import package05.MAV;
 import package05.AgriculturalDrone;
+
 public class Driver {
 	public static <T extends Airplane> Airplane[] copyFlyingObjects(T[] flyingObjects) {
 
@@ -25,8 +26,8 @@ public class Driver {
 		Quadcopter quadcopter1 = new Quadcopter("DJI", 2000.0, 100, 4, 2023, 1, 60);
 		Multirotor multirotor1 = new Multirotor("Parrot", 1000.0, 50, 4, 2023, 1, 4);
 		UAV uav1 = new UAV("brand1", 1.5, 0, 5000.0);
-		AgriculturalDrone agriculturalDrone1 = new AgriculturalDrone("Yamaha", 10000.0, 1500, 2.0,10);
-		MAV mav1 = new MAV("Hoverfly",0.1, 0, 500.0, "Cybertrunk", 0.05);
+		AgriculturalDrone agriculturalDrone1 = new AgriculturalDrone("Yamaha", 10000.0, 1500, 2.0, 10);
+		MAV mav1 = new MAV("Hoverfly", 0.1, 0, 500.0, "Cybertrunk", 0.05);
 
 		Airplane airplane2 = new Airplane();
 		Helicopter helicopter2 = new Helicopter();
@@ -42,9 +43,13 @@ public class Driver {
 				airplane2, helicopter2, quadcopter2, multirotor2, uav2, agriculturalDrone2, mav2, airplane3, mav3,
 				helicopter3 };
 		copyFlyingObjects(flyingObject1);
-		
+		System.out.println("This is original array:");
 		for (Airplane object : flyingObject1) {
 			System.out.println(object);
+		}
+		System.out.println("\nThis is copy array:");
+		for (int i = 0; i < copyFlyingObjects(flyingObject1).length; i++) {
+			System.out.println(copyFlyingObjects(flyingObject1)[i]);
 		}
 	}
 
