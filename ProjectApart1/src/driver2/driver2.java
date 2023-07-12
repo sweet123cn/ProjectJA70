ab42a4c1b32b5be30ce65f1f2048bab3e3adc092
lisiCAO <caolisi@hotmail.com>
2023-07-12 01:16:49 -0400

    fix error in driver

--- ProjectApart1/src/driver2/driver2.java
+++ ProjectApart1/src/driver2/driver2.java
@@ -1,7 +1,12 @@
 package driver2;
 
 import package01.Airplane;
-
+import package02.Helicopter;
+import package02.Quadcopter;
+import package03.Multirotor;
+import package04.UAV;
+import package05.MAV;
+import package05.AgriculturalDrone;
 public class driver2 {
 	public static <T extends Airplane> Airplane[] copyFlyingObjects(T[] flyingObjects) {
 
@@ -19,9 +24,9 @@ public class driver2 {
 		Helicopter helicopter1 = new Helicopter("Bell", 100000.0, 800, 6, 2022, 5);
 		Quadcopter quadcopter1 = new Quadcopter("DJI", 2000.0, 100, 4, 2023, 1, 60);
 		Multirotor multirotor1 = new Multirotor("Parrot", 1000.0, 50, 4, 2023, 1, 4);
-		UAV uav1 = new UAV(1.5, 5000.0);
-		AgriculturalDrone agriculturalDrone1 = new AgriculturalDrone(2.0, 10000.0, "Yamaha", 10);
-		MAV mav1 = new MAV(0.1, 500.0, "Hoverfly", 0.05);
+		UAV uav1 = new UAV("brand1", 1.5, 0, 5000.0);
+		AgriculturalDrone agriculturalDrone1 = new AgriculturalDrone("Yamaha", 10000.0, 1500, 2.0,10);
+		MAV mav1 = new MAV("Hoverfly",0.1, 0, 500.0, "Cybertrunk", 0.05);
 
 		Airplane airplane2 = new Airplane();
 		Helicopter helicopter2 = new Helicopter();
