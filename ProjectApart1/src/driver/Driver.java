c782d07fee19e33cf7209e275e679a0290a93b68
DavidTang <sweet123cn@outlook.com>
2023-07-12 16:29:01 -0400

    update Driver program
    add comment

--- ProjectApart1/src/driver/Driver.java
+++ ProjectApart1/src/driver/Driver.java
@@ -10,18 +10,24 @@ import package05.AgriculturalDrone;
 
 public class Driver {
 
+	//findLeastAndMostExpensiveUAV() to find the least and most expensive object  
+	//if there is only one object, then it is the least and the most expensive object the same time
 	public static <T extends Airplane> void findLeastAndMostExpensiveUAV(T[] flyingObjects) {
 		T leastExpensive = null;
 		T mostExpensive = null;
 		for (int i = 0; i < flyingObjects.length; i++) {
+			//save the least expensive object
 			if (leastExpensive == null || flyingObjects[i].getPrice() < leastExpensive.getPrice()) {
 				leastExpensive = flyingObjects[i];
 			}
+
+			//save the most expensive object
 			if (mostExpensive == null || flyingObjects[i].getPrice() > mostExpensive.getPrice()) {
 				mostExpensive = flyingObjects[i];
 			}
 		}
 
+		//out put the result
 		if (leastExpensive != null) {
 			System.out.println("The least expensive flying object is: " + leastExpensive.getBrand() + " the price is $" + leastExpensive.getPrice());
 		} else {
@@ -35,7 +41,9 @@ public class Driver {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		//make the instance to objects
+		//parameter constructor
 		Airplane airplane1 = new Airplane("Boeing", 150000.0, 1200);
 		Helicopter helicopter1 = new Helicopter("Bell", 100000.0, 800, 6, 2022, 5);
 		Quadcopter quadcopter1 = new Quadcopter("DJI", 2000.0, 100, 4, 2023, 1, 60);
@@ -44,6 +52,7 @@ public class Driver {
 		AgriculturalDrone agriculturalDrone1 = new AgriculturalDrone("Yamaha", 10000.0, 500, 2.0,10);
 		MAV mav1 = new MAV("Hoverfly",299.0, 0, 100.0, "Cybertrunk", 20);
 
+		//default constructor
 		Airplane airplane2 = new Airplane();
 		Helicopter helicopter2 = new Helicopter();
 		Quadcopter quadcopter2 = new Quadcopter();
@@ -51,6 +60,8 @@ public class Driver {
 		UAV uav2 = new UAV();
 		AgriculturalDrone agriculturalDrone2 = new AgriculturalDrone();
 		MAV mav2 = new MAV();
+
+		//copy constructor
 		MAV mav3 = new MAV(mav2);
 		Airplane airplane3 = new Airplane(airplane1);
 		Helicopter helicopter3 = new Helicopter(helicopter2);
@@ -59,12 +70,13 @@ public class Driver {
 		UAV uav3 = new UAV(uav1);
 		AgriculturalDrone agriculturalDrone3 = new AgriculturalDrone(agriculturalDrone2);
 
+		//output all objects
 		System.out.println(airplane1 + "\n" + helicopter1 + "\n" + quadcopter1 + "\n" + multirotor1 + "\n" + uav1 + "\n"
 				+ agriculturalDrone1 + "\n" + mav1 + "\n" + airplane2 + "\n" + helicopter2 + "\n" + quadcopter2 + "\n"
 				+ multirotor2 + "\n" + uav2 + "\n" + agriculturalDrone2 + "\n" + mav2 + "\n" + airplane3 + "\n" + helicopter3 + "\n" + quadcopter3 + "\n" + multirotor3 + "\n" + uav3 + "\n"
 				+ agriculturalDrone3 + "\n" + mav3 + "\n");
 
-		// same class and similar values
+		// equals():same class and similar values
 		if (airplane1.equals(airplane3)) {
 			System.out.println("airplane1 is equals to airplane3");
 		} else {
@@ -77,7 +89,7 @@ public class Driver {
 			System.out.println("quadcopter3 is not equals to quadcopter1");
 		}
 
-		// different class
+		// equals():different class
 		if (airplane1.equals(helicopter1)) {
 			System.out.println("airplane1 is equals to helicopter1 ");
 		} else {
@@ -90,7 +102,7 @@ public class Driver {
 			System.out.println("uav1 is not equals to quadcopter1");
 		}
 
-		// same class and different values
+		// equals():same class and different values
 		if (agriculturalDrone1.equals(agriculturalDrone3)) {
 			System.out.println("agriculturalDrone1 is equals to agriculturalDrone3");
 		} else {
@@ -103,7 +115,7 @@ public class Driver {
 			System.out.println("multirotor3 is not equals to multirotor1");
 		}
 
-
+		//save objects into the array with UAV
 		Airplane[] flyingObject1 = { airplane1, helicopter1, quadcopter1, multirotor1, uav1, agriculturalDrone1, mav1,
 				airplane2, helicopter2, quadcopter2, multirotor2, uav2, agriculturalDrone2, mav1, airplane3, mav3,
 				helicopter3 };
@@ -113,6 +125,7 @@ public class Driver {
 				airplane2, helicopter2, quadcopter2, multirotor2, agriculturalDrone2, mav1, airplane3, mav3,
 				helicopter3 };
 
+		//call findLeastAndMostExpensiveUAV()
 		findLeastAndMostExpensiveUAV(flyingObject1);
 		findLeastAndMostExpensiveUAV(flyingObject2);
 
--- ProjectApart2/src/driver/Driver.java
+++ ProjectApart2/src/driver/Driver.java
@@ -7,6 +7,8 @@ import package03.Multirotor;
 import package04.UAV;
 import package05.MAV;
 import package05.AgriculturalDrone;
+
+//copyFlyingObjects(): pass array of objects, and return the new array
 public class Driver {
 	public static <T extends Airplane> Airplane[] copyFlyingObjects(T[] flyingObjects) {
 
@@ -19,7 +21,9 @@ public class Driver {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//make the instance to objects
+		//parameter constructor
 		Airplane airplane1 = new Airplane("Boeing", 150000.0, 1200);
 		Helicopter helicopter1 = new Helicopter("Bell", 100000.0, 800, 6, 2022, 5);
 		Quadcopter quadcopter1 = new Quadcopter("DJI", 2000.0, 100, 4, 2023, 1, 60);
@@ -28,6 +32,7 @@ public class Driver {
 		AgriculturalDrone agriculturalDrone1 = new AgriculturalDrone("Yamaha", 10000.0, 500, 2.0,10);
 		MAV mav1 = new MAV("Hoverfly",399.0, 0, 100.0, "Cybertrunk", 20);
 
+		//default constructor
 		Airplane airplane2 = new Airplane();
 		Helicopter helicopter2 = new Helicopter();
 		Quadcopter quadcopter2 = new Quadcopter();
@@ -35,6 +40,8 @@ public class Driver {
 		UAV uav2 = new UAV();
 		AgriculturalDrone agriculturalDrone2 = new AgriculturalDrone();
 		MAV mav2 = new MAV();
+
+		//copy constructor
 		MAV mav3 = new MAV(mav2);
 		Airplane airplane3 = new Airplane(airplane1);
 		Helicopter helicopter3 = new Helicopter(helicopter2);
@@ -43,6 +50,7 @@ public class Driver {
 		UAV uav3 = new UAV(uav1);
 		AgriculturalDrone agriculturalDrone3 = new AgriculturalDrone(agriculturalDrone2);
 
+		//output all objects
 		System.out.println(airplane1 + "\n" + helicopter1 + "\n" + quadcopter1 + "\n" + multirotor1 + "\n" + uav1 + "\n"
 				+ agriculturalDrone1 + "\n" + mav1 + "\n" + airplane2 + "\n" + helicopter2 + "\n" + quadcopter2 + "\n"
 				+ multirotor2 + "\n" + uav2 + "\n" + agriculturalDrone2 + "\n" + mav2 + "\n" + airplane3 + "\n" + helicopter3 + "\n" + quadcopter3 + "\n" + multirotor3 + "\n" + uav3 + "\n"
@@ -52,16 +60,17 @@ public class Driver {
 				airplane2, helicopter2, quadcopter2, multirotor2, uav2, agriculturalDrone2, mav2, airplane3, mav3,
 				helicopter3 };
 
+		//output source objects
 		System.out.println("========================================");
-		
 		for (Airplane object : flyingObject1) {
 			System.out.println(object);
 		}
 		
+		//call copyFlyingObjects method
 		Airplane[] copyResultArray = copyFlyingObjects(flyingObject1);
 
+		//output result objects
 		System.out.println("========================================");
-		
 		for (Airplane object : copyResultArray) {
 			System.out.println(object);
 		}
